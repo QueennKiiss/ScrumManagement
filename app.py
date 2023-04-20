f66aae9c8d314840ed054692cbb627b1f0d41674
mmaestro <mmaestroizquierdo@gmail.com>
2023-04-20 11:41:39 +0200

    feat: Modify app implementation to have a more simple app entry point
    This implementation reduce de lines of code and simplify the calls inside this module. The rest of the backend actions are located in specific modules where Blueprints(flask) are created to included in the app.

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 """
 Main module to be executed when deployment
 """
+
+from flask import Flask, render_template, request, flash
+import backlog
+import login
+
+
+app = Flask(__name__)
+app.secret_key = "manbearpig_MUDMAN888"
+
+app.register_blueprint(backlog.bp)
+app.register_blueprint(login.bp)
+
+# Makes url_for('index') == url_for('login.index')
+# In another app, you might define a separate main index here with
+# app.route, while giving the blog blueprint an url_prefix
+app.add_url_rule("/", endpoint="login")
