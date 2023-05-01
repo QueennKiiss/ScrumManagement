3762bf0d01bf487dcb09df663ea1131491f7b987
mmaestro <mmaestroizquierdo@gmail.com>
2023-05-01 21:08:28 +0200

    feat: implement backlog backend module

--- backlog.py
+++ backlog.py
@@ -5,13 +5,52 @@ Contains the Backend of the backlog tab
 from flask import Blueprint
 from flask import render_template
 
-bp = Blueprint("backlog", __name__, url_prefix="/backlog")
+bp = Blueprint("backlog", __name__)
 
 
 @bp.route("/")
-def show_backlog_board() -> render_template:
+def base_page() -> render_template:
     """
     show the backlog page
     """
     return render_template("base.html")
 
+
+@bp.route("/home")
+def show_home_content() -> render_template:
+    """
+    show the backlog page
+    """
+    return render_template("home.html")
+
+
+@bp.route("/backlog")
+def show_backlog_board() -> render_template:
+    """
+    show the backlog page
+    """
+    return render_template("backlog.html")
+
+
+@bp.route("/sprint")
+def show_sprint_content() -> render_template:
+    """
+    show the backlog page
+    """
+    return render_template("sprint.html")
+
+
+@bp.route("/users")
+def show_users_content() -> render_template:
+    """
+    show the backlog page
+    """
+    return render_template("users.html")
+
+
+@bp.route("/board")
+def show_board_content() -> render_template:
+    """
+    show the backlog page
+    """
+    return render_template("board.html")
